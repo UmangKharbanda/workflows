a408b70bcf18210f055699d3ecaeea73c720ff76
umang kharbanda <ukharbanda@yahoo.co.in>
2014-12-17 17:43:15 +0530

    Compressed HTML Files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
 	minifyCSS = require('gulp-minify-css'),
+	minifyHTML = require('gulp-minify-html'),
 	concat = require('gulp-concat');
 
 // VARIABLES
@@ -77,7 +78,7 @@ gulp.task('watch', function(){
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
-	gulp.watch(htmlSources, ['html']);
+	gulp.watch('builds/development/*.html', ['html']);
 	gulp.watch(jsonSources, ['json']);
 });
 
@@ -89,7 +90,9 @@ gulp.task('connect', function(){
 });
 
 gulp.task('html', function(){
-	gulp.src(htmlSources)
+	gulp.src('builds/development/*.html')
+	.pipe(gulpif(env === 'production', minifyHTML()))
+	.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
 	.pipe(connect.reload());
 });
 
